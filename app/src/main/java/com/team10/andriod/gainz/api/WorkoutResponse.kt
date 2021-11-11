4a6a348e44976419ffbb2e43753e4dd41f7e3976
Alex Lowe <loweman64@outlook.com>
2021-11-11 12:43:32 -0500

    fix: Remove Misspelled Package Name

--- app/src/main/java/com/team10/andriod/gainz/api/WorkoutResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.team10.andriod.gainz.api
-
-import com.google.gson.annotations.SerializedName
-
-class WorkoutResponse {
-  @SerializedName("results")
-  var results = ArrayList<Workout>()
-}
