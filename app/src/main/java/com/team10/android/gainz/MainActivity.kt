79890f7f2f5f064e2d5f68cee6c6be88ed46f364
Alex Lowe <loweman64@outlook.com>
2021-11-11 14:53:09 -0500

    feat: Create Basic Hamburger Navigation Menu
    When the menu item is clicked it will display a toast letting the user know that they clicked it.

--- app/src/main/java/com/team10/android/gainz/MainActivity.kt
+++ app/src/main/java/com/team10/android/gainz/MainActivity.kt
@@ -1,44 +1,45 @@
 package com.team10.android.gainz
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import com.google.android.material.navigation.NavigationView
 import com.team10.android.gainz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var toggle: ActionBarDrawerToggle
   private lateinit var navController: NavController
   private lateinit var binding: ActivityMainBinding
+  private lateinit var drawerLayout: DrawerLayout
+  private lateinit var navigationView: NavigationView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-//    binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(R.layout.activity_main)
+    drawerLayout = findViewById(R.id.drawer_layout)
+    toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+    drawerLayout.addDrawerListener(toggle)
+    toggle.syncState()
 
-//    val navHostFragment = supportFragmentManager
-//      .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//    navController = navHostFragment.navController
-//    setupBottomNav(navController)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    navigationView = findViewById(R.id.nav_view)
+    navigationView.setNavigationItemSelectedListener {
+      when(it.itemId) {
+        R.id.workoutItem -> Toast.makeText(applicationContext, "Workout Item Clicked", Toast.LENGTH_SHORT).show()
+      }
+      true
+    }
+
   }
 
-//  private fun setupBottomNav(navController: NavController) {
-//    val bottomNav = binding.bottomNavigation
-//    bottomNav.setupWithNavController(navController)
-//
-//    /*
-//    The set of destinations by id considered at the top level of your information hierarchy.
-//    The Up button will not be displayed when on these destinations.
-//     */
-//    val appBarConfig = AppBarConfiguration(
-//      setOf(
-//        R.id.nav_flow
-//      )
-//    )
-//    /*
-//   By calling this method, the title in the action bar will automatically be updated when
-//   the destination changes
-//    */
-//    setupActionBarWithNavController(navController, appBarConfig)
-//
-//
-//  }
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (toggle.onOptionsItemSelected(item)) {
+      return true
+    }
+    return super.onOptionsItemSelected(item)
+  }
 }
