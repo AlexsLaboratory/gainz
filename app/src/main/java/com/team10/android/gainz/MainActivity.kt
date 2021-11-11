1ef23508b8cd65be6594d8b582ecc47ed9341c4d
Alex Lowe <loweman64@outlook.com>
2021-11-11 15:16:36 -0500

    feat: Replace Currently Shown Fragment when "Workouts" Menu Item is Pressed

--- app/src/main/java/com/team10/android/gainz/MainActivity.kt
+++ app/src/main/java/com/team10/android/gainz/MainActivity.kt
@@ -2,44 +2,50 @@ package com.team10.android.gainz
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
 import com.google.android.material.navigation.NavigationView
-import com.team10.android.gainz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-  private lateinit var toggle: ActionBarDrawerToggle
-  private lateinit var navController: NavController
-  private lateinit var binding: ActivityMainBinding
-  private lateinit var drawerLayout: DrawerLayout
-  private lateinit var navigationView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    drawerLayout = findViewById(R.id.drawer_layout)
-    toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-    drawerLayout.addDrawerListener(toggle)
-    toggle.syncState()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-    supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    navigationView = findViewById(R.id.nav_view)
-    navigationView.setNavigationItemSelectedListener {
-      when(it.itemId) {
-        R.id.workoutItem -> Toast.makeText(applicationContext, "Workout Item Clicked", Toast.LENGTH_SHORT).show()
-      }
-      true
+        val workoutFragment = WorkoutFragment()
+//        supportFragmentManager.beginTransaction().apply {
+//            replace(R.id.fragment_container_view, workoutFragment)
+//            .addToBackStack(null)
+//            .commit()
+//        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.workoutItem -> supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.fragment_container_view, workoutFragment)
+                        .addToBackStack(null)
+                        .commit()
+                }
+            }
+            true
+        }
+
     }
 
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    if (toggle.onOptionsItemSelected(item)) {
-      return true
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
-    return super.onOptionsItemSelected(item)
-  }
 }
